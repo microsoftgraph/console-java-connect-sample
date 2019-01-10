2d659a0cef8884e3dc51055bafdeedb770ce20f3
Jason Johnston <jasonjoh@users.noreply.github.com>
2019-01-10 10:11:23 -0500

    More linter issues
    Added `serialVersionUID` to `SendMailException`.

--- src/main/java/com/microsoft/graphsample/msgraph/SendMailException.java
+++ src/main/java/com/microsoft/graphsample/msgraph/SendMailException.java
@@ -1,6 +1,7 @@
 package com.microsoft.graphsample.msgraph;
 
 public class SendMailException extends Exception {
+    private static final long serialVersionUID = 1L;
     public SendMailException() { super(); }
     public SendMailException(String message) { super(message); }
     public SendMailException(String message, Throwable cause) { super(message, cause); }
